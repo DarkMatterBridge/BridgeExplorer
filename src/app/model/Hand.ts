3a1d2b9f02e480cf56a56df457e4bd1a6ba76d9c
Peter <pfreche@gmail.com>
2021-04-16 14:03:27 +0200

    hand methods

--- src/app/model/Hand.ts
+++ src/app/model/Hand.ts
@@ -9,13 +9,14 @@ export class Hand {
   static valueDE = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'B', 'D', 'K', 'A'];
   static valueEN = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 
-//  cardsInSuit: number[];
+  cardsInSuit: number[];
 
   constructor() {
     this.cards = new Array<Array<string>>();
+    this.cardsInSuit = [0, 0, 0, 0];
   }
 
-  setHandFromString(handString: string) { // Assuming currently only Lin Format
+  setHandFromString(handString: string) { // Assuming currently the Lin Format
     this.setSuitFromString("S", (handString.match(/S(.*?)H/) || []) [0].split(/[SH]/)[1]);
     this.setSuitFromString("H", (handString.match(/H(.*?)D/) || []) [0].split(/[HD]/)[1]);
     this.setSuitFromString("D", (handString.match(/D(.*?)C/) || []) [0].split(/[DC]/)[1]);
@@ -49,6 +50,48 @@ export class Hand {
 
   pintHand(): string {
     return "";
+  }
+
+  detCardsInSuit() {
+    this.cardsInSuit = [0, 0, 0, 0];
+    this.cardsInSuit[0] = this.cards[0].length;
+    this.cardsInSuit[1] = this.cards[1].length;
+    this.cardsInSuit[2] = this.cards[2].length;
+    this.cardsInSuit[3] = this.cards[3].length;
+  }
+
+  isBalanced(): boolean {
+    // 5332 90   4221 16
+    // 4333 108  3222 24
+    // 4432 96   3321 18
+    // 6322 72   5211 10
+    // 5422 80   4311 12
+    if (this.cardsInSuit == null)
+      this.detCardsInSuit();
+    return this.cardsInSuit[3] * this.cardsInSuit[2] * this.cardsInSuit[1] * this.cardsInSuit[0] > 89;
+
+  }
+
+  isSemiBalanced(): boolean {
+    if (this.cardsInSuit == null)
+      this.detCardsInSuit();
+    return this.cardsInSuit[3] * this.cardsInSuit[2] * this.cardsInSuit[1] * this.cardsInSuit[0] > 71;
+  }
+
+  pointsInSuit(suit: number) {
+    this.cards[suit].reduce((a, b) => a + this.points(b), 0);
+  }
+
+  points(c: string) : number{
+    if (c === 'A')
+      return 4;
+    if (c === 'K')
+      return 3;
+    if (c === 'Q')
+      return 2;
+    if (c === 'J')
+      return 1;
+    return 0
   }
 
 }
