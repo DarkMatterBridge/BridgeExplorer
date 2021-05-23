2d53de72d7bfb7bae1c9f99db8b18db0cd22acee
Peter <pfreche@gmail.com>
2021-05-23 09:12:47 +0200

    some (fruitless) trials

--- src/app/cross-origin.service.ts
+++ src/app/cross-origin.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,16 +10,23 @@ export class CrossOriginService {
 
   function: Function = (s: string) => s;
 
+
   constructor(private httpClient: HttpClient) {
     window.addEventListener('message', e => this.processResponse(e));
   }
 
-  loadFromUrlViaMessage(url: string, f: Function) {
+  loadFromUrlViaMessage(url: string, f: Function): void {
     this.function = f;
     window.postMessage({
       direction: 'from-page-script',
       message: url
     }, '*');
+
+    chrome.runtime.getManifest();
+    chrome.runtime.sendMessage('get-user-data', (response: any) => {
+      // 3. Got an asynchronous response with the data from the background
+      console.log('received user data', response);
+    });
   }
 
   loadDirectFromUrl(url: string): Observable<any> {
